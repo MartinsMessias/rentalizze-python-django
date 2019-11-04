103148a16deeacd685c9f7042a0f5966dabe1fe3
martinsmessias <messiasads2017@gmail.com>
2019-11-04 10:24:58 -0500

    Trabalhando templates

--- sistema/views.py
+++ sistema/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def teste(request):
     test = {'page': 'Homepage'}
     return render(request, 'sistema/index.html', {'test': test})
+
 
 def cadastrar_cliente(request):
     # TEST
