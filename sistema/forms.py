25d48b7859c626906f7a3dc61e01ce78c887a93c
martinsmessias <messiasads2017@gmail.com>
2019-11-06 14:28:28 -0500

    Alterações no forms

--- sistema/forms.py
+++ sistema/forms.py
@@ -25,3 +25,10 @@ class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = '__all__'
+
+
+class AutomovelForm(forms.Form):
+
+    class Meta:
+        model = Automovel
+        fields = '__all__'
