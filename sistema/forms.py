7e386755bc0066afcff76dde05e73706afddc506
martinsmessias <messiasads2017@gmail.com>
2019-11-06 14:25:18 -0500

    Alterações no forms

--- sistema/forms.py
+++ sistema/forms.py
@@ -11,7 +11,7 @@ class ClienteForm(forms.ModelForm):
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'email@email.com'}))
     rg_cliente = forms.CharField(widget=forms.NumberInput(attrs={'class': 'form-control', 'min': '1'}))
     cnpj_cliente = forms.CharField(required=False, widget=forms.TextInput(
-        attrs={'class': 'form-control cnpj-inputmask', 'id': 'cnpj_cliente', 'onkeyup': 'validarCNPJ(this);'}), initial='NULL')
+        attrs={'class': 'form-control cnpj-inputmask', 'id': 'cnpj_cliente', 'onkeyup': 'validarCNPJ(this);'}), initial='1')
     cnh_cliente = forms.CharField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
     validade_cnh = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'date', 'class': 'form-control'}))
     rua = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
@@ -24,6 +24,4 @@ class ClienteForm(forms.ModelForm):
 
     class Meta:
         model = Cliente
-        fields = ('nome_cliente', 'cpf_cliente', 'telefone_cliente',
-                  'email_cliente', 'rg_cliente', 'cnh_cliente', 'cnpj_cliente',
-                  'validade_cnh', 'rua', 'numero', 'complemento', 'cep', 'bairro', 'estado', 'cidade', 'status')
+        fields = '__all__'
