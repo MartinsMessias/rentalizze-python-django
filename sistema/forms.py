1dbabdb5c439c6af9e390787260f2838ec3736e9
martinsmessias <messiasads2017@gmail.com>
2019-11-06 11:01:35 -0500

    Alterações

--- sistema/forms.py
+++ sistema/forms.py
@@ -1,17 +1,18 @@
 from django import forms
 from .models import *
 
-
 class ClienteForm(forms.ModelForm):
     nome_cliente = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     cpf_cliente = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control cpf-inputmask', 'id':'cpf_cliente', 'onkeyup':'TestaCPF(this);'}))
     telefone_cliente = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control phone-inputmask'}))
     email_cliente = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'email@email.com'}))
-    rg_cliente = forms.CharField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    cnpj_cliente = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control cnpj-inputmask', 'id':'cpf_cliente', 'onkeyup':'ValidarCNPJ(this);'}))
+    rg_cliente = forms.CharField(widget=forms.NumberInput(attrs={'class': 'form-control', 'min':'1'}))
+    cnpj_cliente = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control cnpj-inputmask', 'id':'cnpj_cliente', 'onkeyup':'validarCNPJ(this);'}))
     cnh_cliente = forms.CharField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
     validade_cnh = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}))
+    #endereco_rua = forms.ModelChoiceField(queryset=Endereco.objects.all())
 
     class Meta:
         model = Cliente
-        fields = ('nome_cliente', 'cpf_cliente', 'telefone_cliente', 'email_cliente', 'rg_cliente', 'cnh_cliente', 'cnpj_cliente', 'validade_cnh')
+        fields = ('nome_cliente', 'cpf_cliente', 'telefone_cliente',
+                  'email_cliente', 'rg_cliente', 'cnh_cliente', 'cnpj_cliente', 'validade_cnh')
