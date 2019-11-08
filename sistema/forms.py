9d21427e8ede38fffc6c6a25ca67682a56a20f63
martinsmessias <messiasads2017@gmail.com>
2019-11-08 10:49:21 -0500

    Alterações em forms, views e templates

--- sistema/forms.py
+++ sistema/forms.py
@@ -5,82 +5,37 @@ from .models import *
 class ClienteForm(forms.ModelForm):
     nome_cliente = forms.CharField(
         widget=forms.TextInput(
-            attrs={'class': 'form-control'}
-        )
-    )
+            attrs={'class': 'form-control'}))
     cpf_cliente = forms.CharField(
         required=False,
         widget=forms.TextInput(
             attrs={'class': 'form-control cpf-inputmask', 'id': 'cpf_cliente', 'onkeyup': 'TestaCPF(this);'}
-        ), initial='00000000000'
-    )
+        ), initial='00000000000')
     telefone_cliente = forms.CharField(
         widget=forms.TextInput(
-            attrs={'class': 'form-control phone-inputmask'}
-        )
-    )
+            attrs={'class': 'form-control phone-inputmask'}))
     email_cliente = forms.CharField(
         widget=forms.TextInput(
-            attrs={'class': 'form-control', 'placeholder': 'email@email.com'}
-        )
-    )
+            attrs={'class': 'form-control', 'placeholder': 'email@email.com'}))
     rg_cliente = forms.CharField(
         widget=forms.NumberInput(
-            attrs={'class': 'form-control', 'min': '0'}
-        )
-    )
+            attrs={'class': 'form-control', 'min': '0'}))
     cnpj_cliente = forms.CharField(
         required=False,
         widget=forms.TextInput(
             attrs={'class': 'form-control cnpj-inputmask', 'id': 'cnpj_cliente', 'onkeyup': 'validarCNPJ(this);'}
-        ), initial='0000000000000000'
-    )
-    cnh_cliente = forms.CharField(
-        widget=forms.NumberInput(
-            attrs={'class': 'form-control'}
-        )
-    )
-    validade_cnh = forms.DateTimeField(
-        widget=forms.DateTimeInput(
-            attrs={'type': 'date', 'class': 'form-control'}
-        )
-    )
-    rua = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'class': 'form-control'}
-        )
-    )
-    numero = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'class': 'form-control', 'min': '0'}
-        )
-    )
-    complemento = forms.CharField(
-        required=False,
-        widget=forms.TextInput(
-            attrs={'class': 'form-control'}
-        )
-    )
-    cep = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'class': 'form-control'}
-        )
-    )
-    bairro = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'class': 'form-control'}
-        )
-    )
-    estado = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'class': 'form-control'}
-        )
-    )
-    cidade = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'class': 'form-control'}
-        )
-    )
+        ), initial='0000000000000000')
+    cnh_cliente = forms.CharField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    validade_cnh = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'date', 'class': 'form-control'}))
+    rua = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    numero = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'min': '0'}))
+    complemento = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    cep = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    bairro = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    estado = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    cidade = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    status = forms.ChoiceField(choices=STATUS_CHOICES,
+                               widget=forms.Select(attrs={'class': 'custom-control custom-select'}))
 
     class Meta:
         model = Cliente
@@ -88,6 +43,8 @@ class ClienteForm(forms.ModelForm):
 
 
 class AutomovelForm(forms.ModelForm):
+    css_class = 'form-control'
+
     class Meta:
         model = Automovel
         exclude = ('criado_em', 'modificado_em',)
