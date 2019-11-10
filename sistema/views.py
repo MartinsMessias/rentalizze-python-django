c5fc179f4c01cdbc6eb13c2340565654555c65ea
martinsmessias <messiasads2017@gmail.com>
2019-11-09 20:56:48 -0500

    Trabalhando templates

--- sistema/views.py
+++ sistema/views.py
@@ -3,20 +3,17 @@ from django.shortcuts import render, redirect
 from .forms import *
 
 
-#################################################
-# As views de CADASTRAR_... devem enviar o form para o template
-# As views de CADASTRAR_... devem realizar o salvamento das informações recebidas pelo template
-#################################################
-
 def index(request):
     return render(request, 'sistema/index.html', locals())
 
 
 def cadastrar_cliente(request):
     form = ClienteForm()
+
     #########################################################
     # Fazer o código de salvar o cliente no banco de dados #
     ########################################################
+
     return render(request, 'sistema/cadastrar_cliente.html', {'form': form})
 
 
