f80ea198aecadd4f44b12713d405c3b85ba6741e
martinsmessias <messiasads2017@gmail.com>
2019-11-14 08:21:51 -0500

    Alterações em views

--- sistema/views.py
+++ sistema/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import *
 
 # Renderiza a página inicial
@@ -12,15 +13,19 @@ def index(request):
 def cadastrar_cliente(request):
     if request.method =='POST':
         form = ClienteForm(request.POST)
+
         if form.is_valid():
             form.save()
+            messages.success(request, 'Cliente cadastrado com sucesso!')
             return redirect(listar_clientes)
-    else:
-        form = ClienteForm()
+        else:
+            messages.warning(request, 'Houve um erro! {}'.format(form.errors))
+
+    form = ClienteForm()
     return render(request, 'sistema/cadastrar_cliente.html', {'form': form})
 
-def listar_clientes(request):
-    dados = Cliente.objects.all()
+def listar_clientes(request):#########################################
+    dados = Cliente.objects.all() # Ordenar por 'criado_em'###########
     return render(request, 'sistema/listar_clientes.html', {'dados':dados})
 
 
@@ -34,9 +39,9 @@ def visualizar_cliente(request, id):
 
 
 ############# VEÍCULO #################
-def cadastrar_veiculo(request):
-    if request.method =='POST':
-        form = AutomovelForm(request.POST)
+def cadastrar_veiculo(request):###################################################
+    if request.method =='POST':## DEIXAR NO MESMO FORMATO DA CADASTRAR CLIENTE ##
+        form = AutomovelForm(request.POST)#######################################
         if form.is_valid():
             form.save()
             return redirect(listar_veiculos)
@@ -63,9 +68,9 @@ def visualizar_veiculo(request, id):
 ############# FIM VEÍCULO #################
 
 ############# LOCAÇÃO #################
-def locar_veiculo(request):
-    if request.method =='POST':
-        form = LocacaoForm(request.POST)
+def locar_veiculo(request):############################################## #
+    if request.method =='POST':### DEIXAR NO FORMATO DA CADASTRAR CLIENTE #
+        form = LocacaoForm(request.POST)################################# #
         if form.is_valid():
             form.save()
             return redirect(listar_locacoes)
