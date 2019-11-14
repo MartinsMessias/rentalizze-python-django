e3f19b985278d82537cd167a3c8842ac89c0c0ba
martinsmessias <messiasads2017@gmail.com>
2019-11-14 09:16:30 -0500

    Alterações em forms e templates

--- sistema/forms.py
+++ sistema/forms.py
@@ -2,24 +2,23 @@ from django import forms
 from .models import *
 from datetime import date, time
 
-STATE_CHOICES = (
-    ('AC', 'Acre'), ('AL', 'Alagoas'), ('AP', 'Amapá'),
-    ('AM', 'Amazonas'), ('BA', 'Bahia'), ('CE', 'Ceará'),
-    ('DF', 'Distrito Federal'), ('ES', 'Espírito Santo'),
-    ('GO', 'Goiás'), ('MA', 'Maranhão'), ('MT', 'Mato Grosso'),
-    ('MS', 'Mato Grosso do Sul'), ('MG', 'Minas Gerais'),
-    ('PA', 'Pará'), ('PB', 'Paraíba'), ('PR', 'Paraná'),
-    ('PE', 'Pernambuco'), ('PI', 'Piauí'), ('RJ', 'Rio de Janeiro'),
-    ('RN', 'Rio Grande do Norte'), ('RS', 'Rio Grande do Sul'),
-    ('RO', 'Rondônia'), ('RR', 'Roraima'), ('SC', 'Santa Catarina'),
-    ('SP', 'São Paulo'), ('SE', 'Sergipe'), ('TO', 'Tocantins')
-)
-
 
 class ClienteForm(forms.ModelForm):
     STATUS_CHOICES = (('Ativo', 'Ativo'), ('Inativo', 'Inativo'))
+    STATE_CHOICES = (
+        ('AC', 'Acre'), ('AL', 'Alagoas'), ('AP', 'Amapá'),
+        ('AM', 'Amazonas'), ('BA', 'Bahia'), ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'), ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'), ('MA', 'Maranhão'), ('MT', 'Mato Grosso'),
+        ('MS', 'Mato Grosso do Sul'), ('MG', 'Minas Gerais'),
+        ('PA', 'Pará'), ('PB', 'Paraíba'), ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'), ('PI', 'Piauí'), ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'), ('RS', 'Rio Grande do Sul'),
+        ('RO', 'Rondônia'), ('RR', 'Roraima'), ('SC', 'Santa Catarina'),
+        ('SP', 'São Paulo'), ('SE', 'Sergipe'), ('TO', 'Tocantins')
+    )
     cpf_cliente = forms.CharField(
-        required=False,
+        required=False, disabled=True,
         widget=forms.TextInput(
             attrs={'class': 'form-control cpf-inputmask', 'id': 'cpf_cliente', 'onkeyup': 'TestaCPF(this);'}
         ), initial='00000000000'
@@ -38,7 +37,7 @@ class ClienteForm(forms.ModelForm):
     )
 
     cnpj_cliente = forms.CharField(
-        required=False,
+        required=False, disabled=True,
         widget=forms.TextInput(
             attrs={'class': 'form-control cnpj-inputmask', 'id': 'cnpj_cliente', 'onkeyup': 'validarCNPJ(this);'}
         ), initial='0000000000000000'
@@ -76,10 +75,14 @@ class AutomovelForm(forms.ModelForm):
 
 
 class LocacaoForm(forms.ModelForm):
-    data_locacao = forms.DateField(widget=forms.DateInput(attrs={'value': date.today(),'type': 'date', 'class': 'form-control'}))
-    hora_locacao = forms.TimeField(widget=forms.TimeInput(attrs={'value': time(),'type': 'time', 'class': 'form-control'}))
-    hora_devolucao = forms.TimeField(widget=forms.TimeInput(attrs={'value': time(), 'type': 'time', 'class': 'form-control'}))
-    data_devolucao = forms.DateField(widget=forms.DateInput(attrs={'value': date.today(), 'type': 'date', 'class': 'form-control'}))
+    data_locacao = forms.DateField(
+        widget=forms.DateInput(attrs={'value': date.today(), 'type': 'date', 'class': 'form-control'}))
+    hora_locacao = forms.TimeField(
+        widget=forms.TimeInput(attrs={'value': time(), 'type': 'time', 'class': 'form-control'}))
+    hora_devolucao = forms.TimeField(
+        widget=forms.TimeInput(attrs={'value': time(), 'type': 'time', 'class': 'form-control'}))
+    data_devolucao = forms.DateField(
+        widget=forms.DateInput(attrs={'value': date.today(), 'type': 'date', 'class': 'form-control'}))
 
     class Meta:
         model = Locacao
