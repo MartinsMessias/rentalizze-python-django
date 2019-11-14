0d0a7baffed7d51c473e21696a30b7f4312c2960
martinsmessias <messiasads2017@gmail.com>
2019-11-13 22:11:50 -0500

    Adição de novos templates e correções

--- sistema/urls.py
+++ sistema/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('cadastrar_cliente/', views.cadastrar_cliente, name='cadastrar_cliente'),
     path('cadastrar_veiculo/', views.cadastrar_veiculo, name='cadastrar_veiculo'),
     path('locar_veiculo/', views.locar_veiculo, name='locar_veiculo'),
-    path('listar_reservas/', views.listar_reservas, name='listar_reservas'),
+    path('listar_locacoes/', views.listar_locacoes, name='listar_locacoes'),
     path('visualizar_loc/<id>', views.visualizar_loc, name='visualizar_loc'),
     path('listar_clientes/', views.listar_clientes, name='listar_clientes'),
 ]
--- sistema/views.py
+++ sistema/views.py
@@ -3,51 +3,56 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import *
 
-
+#
 def index(request):
     return render(request, 'sistema/index.html', locals())
 
+####### CLIENTE
 def cadastrar_cliente(request):
-    form = ClienteForm()
     if request.method =='POST':
         form = ClienteForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(cadastrar_cliente)
+            return redirect(listar_clientes)
     else:
         form = ClienteForm()
-
     return render(request, 'sistema/cadastrar_cliente.html', {'form': form})
 
 def listar_clientes(request):
     dados = Cliente.objects.all()
     return render(request, 'sistema/listar_clientes.html', {'dados':dados})
+###### FIM CLIENTE
 
+####### VEÍCULO
 def cadastrar_veiculo(request):
-    form = AutomovelForm()
     if request.method =='POST':
         form = AutomovelForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(cadastrar_veiculo)
+            return redirect(listar_veiculos)
     else:
         form = AutomovelForm()
 
     return render(request, 'sistema/cadastrar_veiculo.html', {'form':form})
 
+def listar_veiculos(request):
+
+    return render(request, 'sistema/listar_veiculos.html')
+
+
+####### FIM VEÍCULO
 def locar_veiculo(request):
-    form = LocacaoForm()
     if request.method =='POST':
         form = LocacaoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(listar_reservas)
+            return redirect(listar_locacoes)
     else:
         form = LocacaoForm()
 
     return render(request, 'sistema/reserva.html', {'form':form})
 
-def listar_reservas(request):
+def listar_locacoes(request):
     dados = Locacao.objects.all()
 
     return render(request, 'sistema/listar_reservas.html', {'dados':dados})
