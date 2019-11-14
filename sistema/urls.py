19861c0849ddeae33eeb088bc4148a7f54e885d5
martinsmessias <messiasads2017@gmail.com>
2019-11-13 22:46:13 -0500

    Adição de novos urls

--- sistema/urls.py
+++ sistema/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('cadastrar_cliente/', views.cadastrar_cliente, name='cadastrar_cliente'),
     path('cadastrar_veiculo/', views.cadastrar_veiculo, name='cadastrar_veiculo'),
-    path('locar_veiculo/', views.locar_veiculo, name='locar_veiculo'),
+    path('cadastrar_locacao/', views.locar_veiculo, name='locar_veiculo'),
+    path('listar_clientes/', views.listar_clientes, name='listar_clientes'),
+    path('listar_veiculos/', views.listar_veiculos, name='listar_veiculos'),
     path('listar_locacoes/', views.listar_locacoes, name='listar_locacoes'),
     path('visualizar_loc/<id>', views.visualizar_loc, name='visualizar_loc'),
-    path('listar_clientes/', views.listar_clientes, name='listar_clientes'),
+    path('visualizar_cli/<id>', views.visualizar_cliente, name='visualizar_cli'),
+    path('visualizar_vei/<id>', views.visualizar_veiculo, name='visualizar_vei'),
 ]
