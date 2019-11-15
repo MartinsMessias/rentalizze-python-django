435b7487f119abd56c6abeb1d6f30b1194d04783
martinsmessias <messiasads2017@gmail.com>
2019-11-15 14:28:01 -0500

    Correções views

--- sistema/views.py
+++ sistema/views.py
@@ -5,11 +5,13 @@ from django.contrib import messages
 from .forms import *
 
 # Renderiza a página inicial
+@login_required
 def index(request):
     return render(request, 'sistema/index.html', locals())
 
 
 ############# CLIENTE #################
+@login_required
 def cadastrar_cliente(request):
     if request.method =='POST':
         form = ClienteForm(request.POST)
@@ -18,27 +20,23 @@ def cadastrar_cliente(request):
             q1 = Cliente.objects.filter(cpf_cliente=form.cleaned_data['cpf_cliente'])
             q2 = Cliente.objects.filter(cnpj_cliente=form.cleaned_data['cnpj_cliente'])
 
-            if not q1:
+            if not q1 or not q2:
                 form.save()
                 messages.success(request, 'Cliente cadastrado com sucesso!')
                 return redirect(listar_clientes)
-
-            if not q2:
-                form.save()
-                messages.success(request, 'Cliente cadastrado com sucesso!')
-                return redirect(listar_clientes)
-
-            messages.warning(request, "Já existe um cliente com este CPF/CNPJ!")
-            return render(request, 'sistema/cadastrar_cliente.html', {'form': form})
+            else:
+                messages.warning(request, "Já existe um cliente com este CPF/CNPJ!")
+                return render(request, 'sistema/cadastrar_cliente.html', {'form': form})
 
     form = ClienteForm()
     return render(request, 'sistema/cadastrar_cliente.html', {'form': form})
 
+@login_required
 def listar_clientes(request):
     dados = Cliente.objects.all().order_by('criado_em')
     return render(request, 'sistema/listar_clientes.html', {'dados':dados})
 
-
+@login_required
 def editar_cliente(request, id):
     cliente = Cliente.objects.get(id=id)
     form = ClienteForm(request.POST or None, instance=cliente)
@@ -50,15 +48,18 @@ def editar_cliente(request, id):
 
     return render(request, 'sistema/editar_cliente.html', {'form': form})
 
+@login_required
 def excluir_cliente(request, id):
     cliente = Cliente.objects.get(id=id)
     cliente.delete()
     messages.success(request, "Cliente excluído com sucesso!")
     return redirect(listar_clientes)
+
 ############# FIM CLIENTE #################
 
 
 ############# VEÍCULO #################
+@login_required
 def cadastrar_veiculo(request):
     if request.method =='POST':
         form = AutomovelForm(request.POST)
@@ -76,11 +77,12 @@ def cadastrar_veiculo(request):
 
     return render(request, 'sistema/cadastrar_veiculo.html', {'form':form})
 
+@login_required
 def listar_veiculos(request):
     dados = Automovel.objects.all().order_by('criado_em')
     return render(request, 'sistema/listar_veiculos.html', {'dados':dados})
 
-
+@login_required
 def editar_veiculo(request, id):
     automovel = Automovel.objects.get(id=id)
     form = AutomovelForm(request.POST or None, instance=automovel)
@@ -92,14 +94,10 @@ def editar_veiculo(request, id):
 
     return render(request, 'sistema/editar_veiculo.html', {'form': form})
 
-def excluir_automovel(request, id):
-    automovel = Automovel.objects.get(id=id)
-    automovel.delete()
-    messages.success(request, "Automovel excluído com sucesso")
-    return redirect(listar_veiculos)
 ############# FIM VEÍCULO #################
 
 ############# LOCAÇÃO #################
+@login_required
 def locar_veiculo(request):
     if request.method =='POST':
         form = LocacaoForm(request.POST)
@@ -114,10 +112,12 @@ def locar_veiculo(request):
     form = LocacaoForm()
     return render(request, 'sistema/reserva.html', {'form':form})
 
+@login_required
 def listar_locacoes(request):
     dados = Locacao.objects.all().order_by('criado_em')
     return render(request, 'sistema/listar_reservas.html', {'dados':dados})
 
+@login_required
 def editar_loc(request, id):
     locacao = Locacao.objects.get(id=id)
     form = ClienteForm(request.POST or None, instance=locacao)
@@ -130,10 +130,8 @@ def editar_loc(request, id):
     return render(request, 'sistema/editar_locacao.html', {'form': form})
 
 
-def excluir_loc(request, id):
-    locaçao = Locacao.objects.get(id=id)
-    locaçao.delete()
-    messages.success(request, "Locação excluída com sucesso!")
-    return redirect(listar_locacoes)
 ############# FIM LOCAÇÃO #################
 
+@login_required
+def accounts(request):
+    return HttpResponse(404)
