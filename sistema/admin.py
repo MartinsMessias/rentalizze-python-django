27baa02a5833693b0160aa548bfe4ead8be4319a
martinsmessias <messiasads2017@gmail.com>
2019-11-18 15:54:10 -0500

    Correções finais

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.apps import apps
 
-app = apps.get_app_config('sistema')
-
-for model_name, model in app.models.items():
-    admin.site.register(model)
+# app = apps.get_app_config('sistema')
+#
+# for model_name, model in app.models.items():
+#     admin.site.register(model)
