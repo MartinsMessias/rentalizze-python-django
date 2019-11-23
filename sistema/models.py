5f4071b6ec0e47544c323cf5935a0fe60c1d0145
martinsmessias <messiasads2017@gmail.com>
2019-11-23 01:38:43 -0500

    Correção models

--- sistema/models.py
+++ sistema/models.py
@@ -93,7 +93,7 @@ class Automovel(models.Model):
     nro_portas_automovel = models.CharField(max_length=10, choices=NRO_PORTAS_AUTOMOVEL_CHOICES)
     tipo_combustivel_automovel = models.CharField(max_length=50, choices=COMBUSTIVEL_CHOICES)
     quilometragem_automovel = models.FloatField()
-    renavam_automovel = models.IntegerField(unique=True)
+    renavam_automovel = models.CharField(max_length=20, unique=True)
     valor_locacao = models.FloatField()
     motor = models.CharField(max_length=3)
     marca = models.CharField(choices=MARCA_CHOICES, max_length=50)
